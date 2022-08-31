29ebb65296acfb0cead6ff5aa858bbaa62231014
Fernandez Ludovic <ldez@users.noreply.github.com>
2022-08-31 11:06:22 +0200

    chore: update linter and CI

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ go 1.17
 // https://github.com/gravitational/trace/compare/14a9a7dd6aaf...v1.1.17
 require (
 	github.com/gravitational/trace v1.1.16-0.20220114165159-14a9a7dd6aaf
-	github.com/stretchr/testify v1.7.1
+	github.com/stretchr/testify v1.8.0
 )
 
 require (
@@ -18,5 +18,5 @@ require (
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
 	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
 	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- predicate.go
+++ predicate.go
@@ -21,42 +21,42 @@ various predicates for configuration, e.g. Latency() > 40 || ErrorRate() > 0.5.
 
 Here's an example of fully functional predicate language to deal with division remainders:
 
-    // takes number and returns true or false
-    type numberPredicate func(v int) bool
+	    // takes number and returns true or false
+	    type numberPredicate func(v int) bool
 
-    // Converts one number to another
-    type numberMapper func(v int) int
+	    // Converts one number to another
+	    type numberMapper func(v int) int
 
-    // Function that creates predicate to test if the remainder is 0
-    func divisibleBy(divisor int) numberPredicate {
-	    return func(v int) bool {
-		    return v%divisor == 0
-        }
-    }
+	    // Function that creates predicate to test if the remainder is 0
+	    func divisibleBy(divisor int) numberPredicate {
+		    return func(v int) bool {
+			    return v%divisor == 0
+	        }
+	    }
 
-    // Function - logical operator AND that combines predicates
-    func numberAND(a, b numberPredicate) numberPredicate {
-        return func(v int) bool {
-            return a(v) && b(v)
-        }
-    }
+	    // Function - logical operator AND that combines predicates
+	    func numberAND(a, b numberPredicate) numberPredicate {
+	        return func(v int) bool {
+	            return a(v) && b(v)
+	        }
+	    }
 
-    p, err := NewParser(Def{
-		Operators: Operators{
-			AND: numberAND,
-		},
-		Functions: map[string]interface{}{
-			"DivisibleBy": divisibleBy,
-		},
-	})
+	    p, err := NewParser(Def{
+			Operators: Operators{
+				AND: numberAND,
+			},
+			Functions: map[string]interface{}{
+				"DivisibleBy": divisibleBy,
+			},
+		})
 
-	pr, err := p.Parse("DivisibleBy(2) && DivisibleBy(3)")
-    if err == nil {
-        fmt.Fatalf("Error: %v", err)
-    }
-    pr.(numberPredicate)(2) // false
-    pr.(numberPredicate)(3) // false
-    pr.(numberPredicate)(6) // true
+		pr, err := p.Parse("DivisibleBy(2) && DivisibleBy(3)")
+	    if err == nil {
+	        fmt.Fatalf("Error: %v", err)
+	    }
+	    pr.(numberPredicate)(2) // false
+	    pr.(numberPredicate)(3) // false
+	    pr.(numberPredicate)(6) // true
 */
 package predicate
 
